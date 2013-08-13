84179f22f9461a03520b3b3c740b714ede0e9c5f
Erin Lawrence <el2891@gmail.com>
2013-08-13 14:29:36 -0500

    heroku test

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -157,7 +157,7 @@ LOGGING = {
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] =  dj_database_url.config(default='postgres://notes_user@localhost:5432/notes_development')
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
